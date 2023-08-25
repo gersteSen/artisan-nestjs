15462cb13e6b72a2c4faeaeb2a0470b21ab40acf
Tobias Gerstenecker <tobias.gerstenecker@marc-cain.de>
2023-08-25 10:09:01 +0200

    Include TypeormModule and ConfigModule

---
+++ api/src/config/database.config.ts
@@ -0,0 +1,10 @@
+﻿export const databaseConfig = () => ({
+  database: {
+    type: process.env.DATABASE_TYPE,
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    database: process.env.DATABASE_DB,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
+  }
+});
